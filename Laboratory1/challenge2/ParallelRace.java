84314c411a29b473bedc01a2352b522a1d629310
Camilo Aguirre <camilo.aguirre-g@mail.escuelaing.edu.co>
2026-08-04 03:49:18 +0200

    feat: validate even list size

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -12,12 +12,22 @@ public class ParallelRace {
                 list -> list.stream()
                             .max(Integer::compareTo)
                             .orElseThrow();
+        // Find the maximun value in the list
         int maximun = findMaximun.apply(numbers);
+        // Check if the maximun is a multiple of 2
         String maximunMultiplyByTwo =
                 maximun % 2 == 0 ? "YES" : "NO";
+
+        // Function to check if the list size is even
+        Function<List<Integer>, Boolean> isListSizeEven =
+                list -> list.size() % 2 == 0;
+
+        // Check if the list size is even
+        boolean listSizeEven = isListSizeEven.apply(numbers);
     
         System.out.println("The maximum value is: " + maximun);
         System.out.println("Is maximun a multiple of 2?? " + maximunMultiplyByTwo);
+        System.out.println("Is the list size even?? " + listSizeEven);
     }
 }
 
