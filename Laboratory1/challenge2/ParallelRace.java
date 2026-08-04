9012ece0e0682a80ecc6c7b08de316d4a9392b0d
Camilo Aguirre <camilo.aguirre-g@mail.escuelaing.edu.co>
2026-08-04 03:01:20 +0200

    feat: implement maximun value lambda

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -1,5 +1,19 @@
 package challenge2;
 
+import java.util.List;
+import java.util.function.Function;
+
 public class ParallelRace {
+    public static void main(String[] args) {
+        
+       List<Integer> numbers = List.of(10,5,20,15);
+
+       Function<List<Integer>, Integer> findMaximun= 
+                list -> list.stream()
+                            .max(Integer::compareTo)
+                            .orElseThrow();
+        int maximun = findMaximun.apply(numbers);
+        System.out.println("The maximum value is: " + maximun);
+    }
 }
 
