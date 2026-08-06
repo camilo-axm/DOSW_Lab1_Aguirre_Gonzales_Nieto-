b1d1e09a6ee980a3ac276b107424a08344986f72
Sara Sofia Gonzalez <sara.gonzalez-m@mail.escuelaing.edu.co>
2026-08-06 09:06:56 -0500

    Update ParallelRace.java

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -2,9 +2,17 @@ package challenge2;
 import java.util.List; /*List=coleccion*/
 
 public class ParallelRace {
-    public static int getMaxNumber(List<Integer> numbers) {
+    public static int getMaxNumber(List<Integer> numbersMax) {
         return numbers.stream()
         .max((a, b) -> Integer.compare(a, b))
-        .orElse(0); /*si esta vacia retorna 0 */
+        .orElse(0); /*if is empty return  0 */
+    }
+    /* get size of the numbers max of list*/
+    public static int getSizeMax (ist<Integer> numbersMax){
+    return numbersMax.size();
+    }
+    public static String isSizeEven(List<Integer> numbersMax) {
+    return (getSizeMax(numbers) % 2 == 0) ? "Sí" : "No";
     }
 }
+
