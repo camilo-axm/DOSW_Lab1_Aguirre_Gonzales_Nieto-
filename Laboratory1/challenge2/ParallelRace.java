7c265d2e4df48518fa352b3a61c85b6802458c6a
Sara Sofia Gonzalez Mora <sara.gonzalez-m@mail.escuelaing.edu.co>
2026-08-05 22:53:55 -0500

    Implemente la funcion para encontrar el numero minimo y el total de elementos

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -1,11 +1,25 @@
 package challenge2;
-import java.util.List; /**List=coleccion*/
+
+import java.util.List;
+
+/** List=coleccion */
 
 public class ParallelRace {
-    public static int getMaxNumber(List<Integer> numbers) {
-        return numbers.stream()
-        .max((a, b) -> Integer.compare(a, b))
-        .orElse(0); /*si esta vacia retorna 0 */
-    }
-}
 
+    public static int getMax(List<Integer> numbers) {
+        return numbers.stream()
+                .max((a, b) -> Integer.compare(a, b))
+                .orElse(0); /* si esta vacia retorna 0 */
+    }
+
+    public static int getMin(List<Integer> numbers) {
+        return numbers.stream()
+                .min((a, b) -> Integer.compare(a, b))
+                .orElse(0);
+    }
+
+    public static int getTotalElements(List<Integer> numbers) {
+        return numbers.size();
+    }
+
+}
