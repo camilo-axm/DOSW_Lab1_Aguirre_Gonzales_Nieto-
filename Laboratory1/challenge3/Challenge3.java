d47105ffc08931efa76bc1187cecbdc5c7a378cb
Camilo-axm <camiloaguirre1023@gmail.com>
2026-08-06 15:07:39 -0500

    commit challenge

--- Laboratory1/challenge3/Challenge3.java
+++ Laboratory1/challenge3/Challenge3.java
@@ -1,4 +1,20 @@
 package challenge3;
 
-public class challenge3 {
+public class Challenge3 {
+
+    public static void main(String[] args) {
+
+        String result = echoMessage("Echo");
+
+        System.out.println(result);
+
+    }
+
+    public static String echoMessage(String message) {
+
+        StringBuffer buffer = new StringBuffer(message);
+
+        return buffer.reverse().toString();
+
+    }
 }
