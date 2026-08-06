e5a27c500e9cfe51194e616c54e017f9487a363b
Sara Sofia Gonzalez Mora <sara.gonzalez-m@mail.escuelaing.edu.co>
2026-08-06 08:58:20 -0500

    update

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -6,19 +6,20 @@ import java.util.List;
 
 public class ParallelRace {
 
-    public static int getMin(List<Integer> numbers) {
-        return numbers.stream()
+    public static int getMin(List<Integer> numbersMin) {
+        return numbersMin.stream()
                 .min((a, b) -> Integer.compare(a, b))
                 .orElse(0);
     }
 
-    public static int getTotalElements(List<Integer> numbers) {
-        return numbers.size();
+    public static int getTotalElements(List<Integer> numbersMin) {
+        return numbersMin.size();
     }
-
+    /*Verify is the number is multiple of two*/
     public static String isMaxDivisibleByTwo(List<Integer> numbers) {
     int max = getMaxnumber(numbers);
     return (max % 2 == 0) ? "Sí" : "No";
-    }
+    
+
 
 }
