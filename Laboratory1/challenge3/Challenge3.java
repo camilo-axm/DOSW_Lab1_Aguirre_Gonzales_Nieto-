7a328057c26ea606682e4d8fb96ae1252b8d8d97
Juan Sebastian Nieto Herrera <ing.sistemas3.14@gmail.com>
2026-08-05 21:55:01 -0500

    Creating a method using StringBuilder

--- Laboratory1/challenge3/Challenge3.java
+++ Laboratory1/challenge3/Challenge3.java
@@ -1,4 +1,19 @@
 package challenge3;
 
-public class challenge3 {
+import java.util.stream.Stream;
+
+public class Challenge3 {
+    public static void main(String[] args) {
+        String result = echoMessage("Echo");
+        System.out.println(result);
+    }
+
+    public static String echoMessage(String message){
+        StringBuilder builder = new StringBuilder();
+        builder.append(message);
+
+        Stream.of(1, 2).forEach(i -> {builder.append(" ").append(message);});
+
+        return builder.toString();
+    }
 }
