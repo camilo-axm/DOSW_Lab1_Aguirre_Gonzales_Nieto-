5741c9bbf415f0455357cf57cef92cc64b944b82
Camilo Aguirre <camilo.aguirre-g@mail.escuelaing.edu.co>
2026-08-06 05:47:56 +0200

    Extract maximum finding logic into getMaxNumber method
    Refactor main method into getMaxNumber method for better modularity.

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -1,33 +1,10 @@
 package challenge2;
-
-import java.util.List;
-import java.util.function.Function;
+import java.util.List; /*List=coleccion*/
 
 public class ParallelRace {
-    public static void main(String[] args) {
-        
-       List<Integer> numbers = List.of(10,5,20,15);
-
-       Function<List<Integer>, Integer> findMaximun= 
-                list -> list.stream()
-                            .max(Integer::compareTo)
-                            .orElseThrow();
-        // Find the maximun value in the list
-        int maximun = findMaximun.apply(numbers);
-        // Check if the maximun is a multiple of 2
-        String maximunMultiplyByTwo =
-                maximun % 2 == 0 ? "YES" : "NO";
-
-        // Function to check if the list size is even
-        Function<List<Integer>, Boolean> isListSizeEven =
-                list -> list.size() % 2 == 0;
-
-        // Check if the list size is even
-        boolean listSizeEven = isListSizeEven.apply(numbers);
-    
-        System.out.println("The maximum value is: " + maximun);
-        System.out.println("Is maximun a multiple of 2?? " + maximunMultiplyByTwo);
-        System.out.println("Is the list size even?? " + listSizeEven);
+    public static int getMaxNumber(List<Integer> numbers) {
+        return numbers.stream()
+        .max((a, b) -> Integer.compare(a, b))
+        .orElse(0); /*si esta vacia retorna 0 */
     }
 }
-
