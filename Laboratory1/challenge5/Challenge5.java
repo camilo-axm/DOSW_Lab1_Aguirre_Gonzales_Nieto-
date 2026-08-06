b718de8bd215d1145754ce9b4fdff8014bbe315a
Juan Sebastian Nieto Herrera <ing.sistemas3.14@gmail.com>
2026-08-06 15:19:38 -0500

    feat: Creating a hash army method

--- Laboratory1/challenge5/Challenge5.java
+++ Laboratory1/challenge5/Challenge5.java
@@ -1,4 +1,32 @@
 package challenge5;
 
-public class challenge5 {
+import java.util.*;;
+
+public class Challenge5 {
+
+    public static void main(String[] args) {
+        HashSet<Integer> result = hashArmy();
+        System.out.println(result);
+    }   
+
+    public static HashSet<Integer> hashArmy() {
+        HashSet<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            int value = random.nextInt(30) + 1;
+            numbers.add(value);
+        }
+        
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()) {
+            int current = iterator.next();
+            if (current % 3 == 0) {
+                iterator.remove();
+            }
+        }
+        
+        return numbers;
+    }
+
 }
