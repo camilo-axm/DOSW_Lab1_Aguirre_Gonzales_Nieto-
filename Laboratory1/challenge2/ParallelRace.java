0bdacd00142b82f5dce9b3bb94fca54d83737266
Sara Sofia Gonzalez Mora <sara.gonzalez-m@mail.escuelaing.edu.co>
2026-08-05 20:44:16 -0500

    cree la primera instruccion del reto 2

--- Laboratory1/challenge2/ParallelRace.java
+++ Laboratory1/challenge2/ParallelRace.java
@@ -1,4 +1,11 @@
 package challenge2;
+import java.util.List; /**List=coleccion*/
 
 public class ParallelRace {
+    public static int getMaxNumber(List<Integer> numbers) {
+        return numbers.stream()
+        .max((a, b) -> Integer.compare(a, b))
+        .orElse(0); /*si esta vacia retorna 0 */
+    }
 }
+
