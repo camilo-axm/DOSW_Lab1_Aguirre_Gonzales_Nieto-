3242f81fb0a40633a2df219aeb04790e697d2e49
Camilo Aguirre <camilo.aguirre-g@mail.escuelaing.edu.co>
2026-08-06 04:04:48 +0200

    resolver merge conflict and complete challenge

--- Laboratory1/challenge1/Challenge1.java
+++ Laboratory1/challenge1/Challenge1.java
@@ -1,37 +1,33 @@
 package challenge1;
 
-<<<<<<< Updated upstream
+import java.util.Arrays;
+import java.util.List;
+
 public class Challenge1 {
-}
-=======
-public class Student {
 
-    private String name;
-    private int age;
-    private String email;
-    private int semester;
+    public static void main(String[] args) {
 
-    public Student(String name, int age, String email, int semester) {
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        this.semester = semester;
-    }
+        List<Student> students = Arrays.asList(
+                new Student(
+                        "Camilo Aguirre",
+                        22,
+                        "camilo.aguirre@escuelaing.edu.co",
+                        7
+                ),
+                new Student(
+                        "Sara Sofia Gonzalez",
+                        21,
+                        "sara.gonzalez-g@escuelaing.edu.co",
+                        7
+                ),
+                new Student(
+                        "Juan David Nieto",
+                        21,
+                        "juan.nieto@escuelaing.edu.co",
+                        7
+                )
+        );
 
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public int getSemester() {
-        return semester;
+        System.out.println(WelcomeMessage.createMessage(students));
     }
 }
->>>>>>> Stashed changes
