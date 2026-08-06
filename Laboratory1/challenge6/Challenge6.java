828136ac0339f7bdd531c693449489e454fe7fd7
Camilo-axm <camiloaguirre1023@gmail.com>
2026-08-06 12:45:17 -0500

    creacion nueva rama con archivo correcto final

--- Laboratory1/challenge6/Challenge6.java
+++ Laboratory1/challenge6/Challenge6.java
@@ -1,4 +1,35 @@
-package challenge6;
+import java.util.HashMap;
+import java.util.Map;
 
-public class challenge6 {
+public class Challenge6 {
+
+    static Map<String, Runnable> commands = new HashMap<>();
+
+    static {
+        commands.put("GREET", () -> System.out.println("Greetings, traveler of time and code!"));
+        commands.put("FAREWELL", () -> System.out.println("May the bits be with you until the next mission."));
+        commands.put("SING", () -> System.out.println("01010101"));
+        commands.put("DANCE", () -> System.out.println("Spinning in party mode."));
+    }
+
+    public static void main(String[] args) {
+        executeCommand("GREET");
+        executeCommand("FAREWELL");
+        executeCommand("SING");
+        executeCommand("DANCE");
+        executeCommand("PARTY");
+    }
+
+    public static void executeCommand(String command) {
+        switch (command) {
+            case "GREET":
+            case "FAREWELL":
+            case "SING":
+            case "DANCE":
+                commands.get(command).run();
+                break;
+            default:
+                System.out.println("Command not recognized.");
+        }
+    }
 }
